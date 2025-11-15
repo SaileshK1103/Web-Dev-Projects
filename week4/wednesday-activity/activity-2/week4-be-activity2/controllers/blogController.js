390054fbb95b94a180b8e1da7be0317d84193011
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-11-15 17:53:20 +0200

    Added error handling with tey-catch to Blog and User Controllers.

--- week4/wednesday-activity/activity-2/week4-be-activity2/controllers/blogController.js
+++ week4/wednesday-activity/activity-2/week4-be-activity2/controllers/blogController.js
@@ -1,61 +1,96 @@
 const Blog = require("../models/blogModel");
+const mongoose = require("mongoose");
 
 // GET /blogs
-const getAllBlogs = async(req,res) => {
-    const blogs = await Blog.find({}).sort({createdAt: -1});
+const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({}).sort({ createdAt: -1 });
     res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve blogs" });
+  }
 };
 
 // POST /blogs
-const createBlog = async (req,res) => {
-   const newBlog = await Blog.create({ ...req.body})
-   res.status(201).json(newBlog);
+const createBlog = async (req, res) => {
+  try {
+    const newBlog = await Blog.create({ ...req.body });
+    res.status(201).json(newBlog);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Failed to create blog", error: error.message });
+  }
 };
 // GET /blogs/:blogId
-const getBlogById = async(req,res) => {
-    const {blogId} = req.params;
+const getBlogById = async (req, res) => {
+  const { blogId } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(blog)) {
+    return res.status(400).json({ message: "Invalid blog ID" });
+  }
+  
+  try {
     const blog = await Blog.findById(blogId);
-    if(blog) {
-        res.status(200).json(blog);
+    if (blog) {
+      res.status(200).json(blog);
     } else {
-        res.status(404).json({message: "Blog not found"});
+      res.status(404).json({ message: "Blog not found" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve blog" });
+  }
 };
 
 // PUT /blogs/:blogId
-const updateBlog = async(req,res) => {
-    const {blogId} = req.params;
+const updateBlog = async (req, res) => {
+  const { blogId } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(blogId)) {
+    return res.status(400).json({ message: "Invalid blog ID" });
+  }
+
+  try {
     const updatedBlog = await Blog.findOneAndUpdate(
-        { _id: blogId },
-        { ...req.body },
-        { new: true }
+      { _id: blogId },
+      { ...req.body },
+      { new: true }
     );
-    if(updatedBlog) {
-        res.status(200).json(updatedBlog);
+    if (updatedBlog) {
+      res.status(200).json(updatedBlog);
     } else {
-        res.status(404).json({message: "Blog not found"});
+      res.status(404).json({ message: "Blog not found" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update blog" });
+  }
 };
 
 // DELETE /blogs/:blogId
-const deleteBlog = async(req, res) => {
-    const {blogId} = req.params;
+const deleteBlog = async (req, res) => {
+  const { blogId } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(blogId)) {
+    return res.status(400).json({ message: "Invalid blog ID" });
+  }
+
+  try {
     const deletedBlog = await Blog.findByIdAndDelete(blogId);
     if (deletedBlog) {
-        res.status(200).json({message: "Blog deleted successfully"});
+      res.status(200).json({ message: "Blog deleted successfully" });
     } else {
-        res.status(404).json({message: "Blog not found"})
+      res.status(404).json({ message: "Blog not found" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete blog" });
+  }
 };
 
 // Export all controller function
 module.exports = {
-    getAllBlogs,
-    getBlogById,
-    createBlog,
-    updateBlog,
-    deleteBlog
-}
+  getAllBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+};
