ecca9bf96f827908604f27fb28c3a179e2e3b03c
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-11-15 18:58:33 +0200

    Added suggetion for improvement to optimize db.js

--- week4/wednesday-activity/activity-2/week4-be-activity2/config/db.js
+++ week4/wednesday-activity/activity-2/week4-be-activity2/config/db.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 
+/* 
+ * Recommendation
+ * Prevent Redundant Connections : leads application constantly trying to establish new connections, 
+ * wasting resources or causing errors.
+ * Best practice to use environment variables
+ * Improved error logis (e.g console.error(error))
+ */
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect("mongodb://localhost:27017/web-dev");
